d140cb34b5f73fa74b67f1c506056e473370c0db
Serge Inacio <sergeinacio@gmail.com>
2017-05-15 22:44:31 +0200

    styled book area

--- wp-content/themes/ausgerechnet-islam/front-page.php
+++ wp-content/themes/ausgerechnet-islam/front-page.php
@@ -47,16 +47,16 @@ get_header( 'home' ); ?>
 			  <div class="vertical-tabs">
 			    <a href="javascript:void(0)" class="js-vertical-tab vertical-tab is-active" rel="tab1">Das Buch</a>
 			    <a href="javascript:void(0)" class="js-vertical-tab vertical-tab" rel="tab2">Leseprobe</a>
-			    <a href="javascript:void(0)" class="js-vertical-tab vertical-tab" rel="tab3">Item 3</a>
+			    <a href="javascript:void(0)" class="js-vertical-tab vertical-tab" rel="tab3">Video trailer</a>
 			    <a href="javascript:void(0)" class="js-vertical-tab vertical-tab" rel="tab4">Item 4</a>
 			  </div>
 
 			  <div class="vertical-tab-content-container">
-			    <a href="" class="js-vertical-tab-accordion-heading vertical-tab-accordion-heading is-active" rel="tab1">Item 1</a>
+			    <a href="" class="js-vertical-tab-accordion-heading vertical-tab-accordion-heading is-active" rel="tab1">Das Buch</a>
 			    <div id="tab1" class="js-vertical-tab-content vertical-tab-content">
 						<div class="col-8">
 							<h4 class="uppercase center-text">Das Buch zum Blog:</h4>
-							<p>"Ein Buch, das Konvertierten „oh-ja!“-, geborenen Muslimen „aha“- und Nichtmuslimen „ach-so!“-Erlebnisse bescheren wird. Und inschaAllah allen einfach viel Freude beim Lesen."</p>
+							<p>Ein Buch, das Konvertierten „oh-ja!“-, geborenen Muslimen „aha“- und Nichtmuslimen „ach-so!“-Erlebnisse bescheren wird. Und inschaAllah allen einfach viel Freude beim Lesen.</p>
 						</div>
 						<div class="col-4-last">
 							<img style="width: 100%; height: auto" src="<?php echo get_stylesheet_directory_uri(); ?>/img/das-buch.jpg" alt="">
@@ -65,16 +65,16 @@ get_header( 'home' ); ?>
 
 			    <a href="" class="js-vertical-tab-accordion-heading vertical-tab-accordion-heading" rel="tab2">Leseprobe</a>
 			    <div id="tab2" class="js-vertical-tab-content vertical-tab-content" style="height: inherit; overflow-y: scroll">
-			      <h6>Es ist ein Rätsel:</h6>
+			      <p><strong>Es ist ein Rätsel:</strong></p>
 						<p>Da ist eine Frau, Schweizerin, bald 60, Sprachlehrerin, mit beiden Füßen fest im Leben stehend, intelligent, emanzipiert, selbständig. Sie lebt mit ihrem Mann in einer wunderschönen Gegend in Südeuropa, hat Haus, Garten, genug Freizeit und viele Hobbies. Mit Religion hat sie nichts am Hut – im Gegenteil, sie ist eingefleischte Agnostikerin und der festen Überzeugung, Glaube sei nur etwas für schlichtere Gemüter.</p>
 						<p>Und plötzlich fängt sie an, von Gott zu faseln. Von Allah. Von Wahrheit.</p>
 						<p>Was ist geschehen? Nein, sie hat keinen muslimischen Liebhaber. Weder ist sie in einer Sekte gelandet, noch ist da jemand, der sie beeinflusst - sie ist auch gar nicht der Typ, der sich leicht für etwas einnehmen ließe, dazu ist sie viel zu eigenständig.</p>
 						<p>Während ihre Angehörigen und Freunde rätseln, ob sie wohl psychologische oder gar psychiatrische Hilfe braucht, beginnt sie, zu bloggen.</p>
 			    </div>
 
-			    <a href="" class="js-vertical-tab-accordion-heading vertical-tab-accordion-heading" rel="tab3">Item 3</a>
+			    <a href="" class="js-vertical-tab-accordion-heading vertical-tab-accordion-heading" rel="tab3">Video trailer</a>
 			    <div id="tab3" class="js-vertical-tab-content vertical-tab-content">
-			      <p>Donec mattis mauris gravida metus laoreet non rutrum sem viverra. Aenean nibh libero, viverra vel vestibulum in, porttitor ut sapien. Phasellus tempor lorem id justo ornare tincidunt. Nulla faucibus, purus eu placerat fermentum, velit mi iaculis nunc, bibendum tincidunt ipsum justo eu mauris. Nulla facilisi. Vestibulum vel lectus ac purus tempus suscipit nec sit amet eros. Nullam fringilla, enim eu lobortis dapibus, quam magna tincidunt nibh, sit amet imperdiet dolor justo congue turpis.</p>
+			      <iframe width="640" height="360" src="https://www.youtube.com/embed/f--zqVa0t4c?rel=0" frameborder="0" allowfullscreen></iframe>
 			    </div>
 
 			    <a href="" class="js-vertical-tab-accordion-heading vertical-tab-accordion-heading" rel="tab4">Item 4</a>
